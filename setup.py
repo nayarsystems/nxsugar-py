02b21225aca1bea4ff5377a52a32b27689485f8f
Javier Sancho <jsf@jsancho.org>
2016-11-03 08:29:34 +0100

    Version changed to 1.3.6

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ with open('README.rst') as f:
 
 setup(
     name='nxsugarpy',
-    version='1.3.5',
+    version='1.3.6',
     description='Build microservices over Nexus',
     long_description=long_description,
     url='https://github.com/nayarsystems/nxsugar-py',
